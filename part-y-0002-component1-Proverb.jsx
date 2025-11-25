a7c13b826c3f412df889776eb9a5f58439a601ea
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-25 13:38:58 +0300

    Create part-y-0002-component1-Proverb.jsx

---
+++ part-y-0002-component1-Proverb.jsx
@@ -0,0 +1,13 @@
+function Proverb() {
+
+    return (
+
+        <>  
+            <div>My proverb:</div>
+            <div>A rolling stone gathers no moss</div>
+        </>
+        
+    )
+}
+
+export default Proverb
