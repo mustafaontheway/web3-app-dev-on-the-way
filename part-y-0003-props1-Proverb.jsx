c7bbfb3b700a1fab52262e0cfc6ebbb11c5a4e77
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-25 13:51:12 +0300

    Create part-y-0003-props1-Proverb.jsx

---
+++ part-y-0003-props1-Proverb.jsx
@@ -0,0 +1,13 @@
+function Proverb({myProverb}) {
+
+    return (
+
+        <>  
+            <div>My proverb:</div>
+            <div>{myProverb}</div>
+        </>
+        
+    )
+}
+
+export default Proverb
