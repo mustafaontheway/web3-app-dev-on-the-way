a40ce877a15655d697e65227ee7f7815c54d0248
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-26 18:50:07 +0300

    Create part-z-0005-function-overloading1.sol

---
+++ part-z-0005-function-overloading1.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    function mult(int x, int y) public pure returns (int) {
+
+        return x * y;
+    }
+
+
+    function mult(int x, int y, int z) public pure returns (int) {
+
+        return x * y * z;
+    }
+
+}
