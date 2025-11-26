6b6f47d0cb82d27ef8142d89b7dc25b6950210e2
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-26 18:35:09 +0300

    Create part-z-0003-function-pure1.sol

---
+++ part-z-0003-function-pure1.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    function sum(int x, int y, int z) public pure returns (int) {
+
+        return x + y + z;
+    }
+
+}
