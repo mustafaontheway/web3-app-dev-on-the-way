a03498283c4af1ff39926a8cad1365261fdb90e0
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-26 19:51:07 +0300

    Update part-z-0011-constructor1.sol

--- part-z-0011-constructor1.sol
+++ part-z-0011-constructor1.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+contract Mustafa {
+
+    uint16 public firstYear;
+
+    address public owner;
+
+    constructor(uint16 _firstYear) {
+
+        firstYear = _firstYear;
+
+        owner = msg.sender;
+    }
+}
